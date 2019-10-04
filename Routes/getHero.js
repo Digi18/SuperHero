5bdd215c33c030ee73ace0d540a208a93be0133e
Digi18 <me.digvijay18@gmail.com>
2019-10-04 12:44:16 +0530

    Added new route getHero.js

---
+++ Routes/getHero.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const dotenv = require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+
+const dburl = process.env.URL;
+
+router.get('/getHero',(req,res) => {
+
+
+  MongoClient.connect(dburl,{useNewUrlPrser:true,useUnifiedTopology:true},(err,client) => {
+
+               if(err){
+               	console.log("Error",err);
+               }else{
+               	
+               }
+
+    });
+
+
+});
+
+module.exports = router;
